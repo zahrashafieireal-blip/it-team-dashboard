24450a270035b479522697ef2b198e6027ff458b
zahrashafieireal-blip <zahrashafiei.real@gmail.com>
2026-09-19 12:32:00 +0330

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
